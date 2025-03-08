32fc614c2dd7c3550d32b058c16af9e890ca8424
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-08 15:59:41 -0600

    add About, Amenities, Contact, Home, and Rooms components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css';
 import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Rooms from './Components/Rooms';
+import Amenities from './Components/Amenities';
+import Contact from './Components/Contact';
 
 function App() {
 
@@ -9,6 +14,11 @@ function App() {
         <header>
           <NavBar />
         </header>
+          <Home />
+          <About />
+          <Rooms />
+          <Amenities />
+          <Contact />
       </div> 
     </>
   )
