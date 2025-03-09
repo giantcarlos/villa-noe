034da6a3e07b37b28e74919bc9a5cb0541c372f3
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-08 19:00:07 -0600

    add home text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,9 +5,8 @@ function Home() {
     <section className="home">
         <div className="home-grid">
             <img className="home-image" alt="Sunrise on Villa Noe Beach sign." src="sunrise.jpg" />
-            <h1>Villa Noe Beach</h1>
-            <h2>City of Mauban</h2>
-            <h2>Quezon Province, Philippines</h2>
+            <h1 className="large-font">Villa Noe Beach</h1>
+            <h2 className="small-font">Quezon Province, Philippines</h2>
         </div>
     </section>
   )
