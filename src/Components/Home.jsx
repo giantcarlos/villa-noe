7afbc19da5a6ee728c34126ed3c8b8361d83b773
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-08 19:19:58 -0600

    edit home text

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
         <div className="home-grid">
             <img className="home-image" alt="Sunrise on Villa Noe Beach sign." src="sunrise.jpg" />
             <h1 className="large-font">Villa Noe Beach</h1>
-            <h2 className="small-font">Quezon Province, Philippines</h2>
+            <h2 className="small-font">Quezon, Philippines</h2>
         </div>
     </section>
   )
