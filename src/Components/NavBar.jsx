e0729baa50b84cbed5d5f119545dd7eb1b947d2b
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-09 16:53:31 -0500

    add animation to nav

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 function NavBar() {
+
+  const reveal = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+}
+
   return (
     <nav>
-      <div className="nav">
+      <motion.div variants={reveal} initial="hidden" animate="show" transition={{duration: 1, delay: 1}} className="nav">
         <img alt="Logo of Villa Noe." className="logo-image" src="logo.png" />
         <div className="numbers">0906-519-7126 / 0921-784-9062</div>
-      </div>
+      </motion.div>
     </nav>
   )
 }
