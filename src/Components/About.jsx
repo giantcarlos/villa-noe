d69110ff4d23cddc00a7aeeaa415fdbd132b4ab9
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-10 17:58:19 -0500

    edit about info

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -27,7 +27,7 @@ function About() {
             <div className="home-grid">
                 <motion.img variants={reveal} initial="hidden" animate="show" transition={{duration: .375, delay:1.7}} className="about-image" alt="Sunrise on Villa Noe Beach sign." src="cagbalete.jpg" />
                 <motion.h2 className="small-font-2" variants={item3} initial="hidden" animate="show" transition={{duration: .25, delay:1.325}}>WELCOME TO VILLA NOE BEACH</motion.h2>
-                <motion.p className="paragraph" variants={item2} initial="hidden" animate="show" transition={{duration: .5, delay:1.375}}>Nestled on the pristine shores of Cagbalete Island in Mauban, Quezon, Villa Noe Beach offers a peaceful retreat with clear waters, white sand, and breathtaking sunsets. Whether you’re here to unwind by the beach, explore the island’s marine life, or simply enjoy the tranquility, our resort provides a comfortable and scenic escape from the everyday.</motion.p>
+                <motion.p className="paragraph" variants={item2} initial="hidden" animate="show" transition={{duration: .5, delay:1.375}}>Nestled on the pristine shores of Cagbalete Island in Mauban, Quezon, Villa Noe Beach offers a peaceful retreat with clear waters, white sand, and breathtaking sunrises. Whether you’re here to unwind by the beach, explore the island’s marine life, or simply enjoy the tranquility, our resort provides a comfortable and scenic escape from the everyday.</motion.p>
                 <motion.button className="button-2" variants={item2} initial="hidden" animate="show" transition={{duration: .5, delay:1.5}}>LEARN MORE</motion.button>
             </div>
         </section>
