401dfc76c1fcb0250e7c21ba46825485ee9ea357
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-10 23:15:38 -0500

    change home image animation direction

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 function Home() {
 
     const reveal = {
-        hidden: { x: 75, opacity: 0 },
-        show: { x: 0, opacity: 1 },
+        hidden: { y: 75, opacity: 0 },
+        show: { y: 0, opacity: 1 },
     }
     const item = {
         hidden: { y: -1000 },
@@ -24,7 +24,7 @@ function Home() {
   return (
     <motion.section variants={item} initial="hidden" animate="show" transition={{duration: 1.25, delay: .125}} className="home">
         <div className="home-grid">
-            <motion.img variants={reveal} initial="hidden" animate="show" transition={{duration: .625, delay: 1.75}}className="home-image" alt="Sunrise on Villa Noe Beach sign." src="sunrise.jpg" />
+            <motion.img variants={reveal} initial="hidden" animate="show" transition={{duration: .625, delay: 1.25}}className="home-image" alt="Sunrise on Villa Noe Beach sign." src="sunrise.jpg" />
             <motion.h2 className="small-font" variants={item3} initial="hidden" animate="show" transition={{duration: .35, delay:1.325}}>Cagbalete Island, Philippines</motion.h2>
             <motion.h1 className="large-font" variants={item2} initial="hidden" animate="show" transition={{duration: .6, delay:1.375}}>VILLA NOE BEACH</motion.h1>
             <motion.button className="button" variants={item2} initial="hidden" animate="show" transition={{duration: .7, delay:1.5}}>BOOK NOW</motion.button>
