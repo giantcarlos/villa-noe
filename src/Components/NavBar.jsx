b73c3cbea65ac36912514c5599cf6ec1f839fcf0
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-10 21:26:48 -0500

    nav in fixed position

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -16,15 +16,14 @@ function NavBar() {
     }
 
   const reveal = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1 },
+    hidden: { y: -100 },
+    show: { y: 0 },
 }
 
   return (
-    <nav>
-      <motion.div variants={reveal} initial="hidden" animate="show" transition={{duration: 1, delay: 1}} className="nav">
+      <motion.nav variants={reveal} initial="hidden" animate="show" transition={{duration: 1.25, delay: 1.125}} className="nav">
         <img alt="Logo of Villa Noe." className="logo-image" src="logo.png" />
-        <div className="numbers">0906-519-7126 / 0921-784-9062</div>
+        <div className="title">VILLA NOE BEACH</div>
         <div className="hamburger" onClick={navToggle}>
                 <Hamburger toggled={isOpen} toggle={setOpen}/>
                 </div>
@@ -35,8 +34,8 @@ function NavBar() {
                     <HashLink smooth to="#merch" className="links" scroll={el => scrollWithOffset(el)}>MERCH ➤</HashLink>
                     <HashLink smooth to="#press" className="links" scroll={el => scrollWithOffset3(el)}>PRESS ➤</HashLink> */}
                 </div>
-      </motion.div>
-    </nav>
+            <div className="numbers">0906-519-7126 / 0921-784-9062</div>
+      </motion.nav>
   )
 }
 
