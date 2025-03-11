1bf2bef1f9f7ccea89985b1d32ee74d0640bb1ed
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-10 21:54:35 -0500

    adjust home animations speeds

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
   return (
     <motion.section variants={item} initial="hidden" animate="show" transition={{duration: 1.25, delay: .125}} className="home">
         <div className="home-grid">
-            <motion.img variants={reveal} initial="hidden" animate="show" transition={{duration: .375, delay:1.7}}className="home-image" alt="Sunrise on Villa Noe Beach sign." src="sunrise.jpg" />
+            <motion.img variants={reveal} initial="hidden" animate="show" transition={{duration: .625, delay: 1.75}}className="home-image" alt="Sunrise on Villa Noe Beach sign." src="sunrise.jpg" />
             <motion.h2 className="small-font" variants={item3} initial="hidden" animate="show" transition={{duration: .25, delay:1.325}}>Cagbalete Island, Philippines</motion.h2>
             <motion.h1 className="large-font" variants={item2} initial="hidden" animate="show" transition={{duration: .5, delay:1.375}}>VILLA NOE BEACH</motion.h1>
             <motion.button className="button" variants={item2} initial="hidden" animate="show" transition={{duration: .5, delay:1.5}}>BOOK NOW</motion.button>
