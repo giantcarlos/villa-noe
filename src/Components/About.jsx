452b93f30be4737f35446b70b3a9d36c2829aa1e
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-11 16:24:59 -0500

    edit about animation speeds

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,10 +6,10 @@ function About() {
   return (
     <section className="about">
             <div className="home-grid">
-                <motion.img initial={{ opacity: 0, y: 75 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.25 }} viewport={{ margin: "-200px", once: true }} className="about-image" alt="Sunrise on Villa Noe Beach sign." src="cagbalete.jpg" />
-                <motion.h2 className="small-font-2" initial={{ opacity: 0, y: 75 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.25 }} viewport={{ margin: "-100px", once: true }} >WELCOME TO VILLA NOE BEACH</motion.h2>
-                <motion.p className="paragraph" initial={{ opacity: 0, y: 75 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.25 }} viewport={{ margin: "-100px", once: true }} >Nestled on the pristine shores of Cagbalete Island in Mauban, Quezon, Villa Noe Beach offers a peaceful retreat with clear waters, white sand, and breathtaking sunrises. Whether you’re here to unwind by the beach, explore the island’s marine life, or simply enjoy the tranquility, our resort provides a comfortable and scenic escape from the everyday.</motion.p>
-                <motion.button className="button-2" initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.25 }} viewport={{ margin: "-50px", once: true }} >LEARN MORE</motion.button>
+                <motion.img initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-200px", once: true }} className="about-image" alt="Sunrise on Villa Noe Beach sign." src="cagbalete.jpg" />
+                <motion.h2 className="small-font-2" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} >WELCOME TO VILLA NOE BEACH</motion.h2>
+                <motion.p className="paragraph" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} >Nestled on the pristine shores of Cagbalete Island in Mauban, Quezon, Villa Noe Beach offers a peaceful retreat with clear waters, white sand, and breathtaking sunrises. Whether you’re here to unwind by the beach, explore the island’s marine life, or simply enjoy the tranquility, our resort provides a comfortable and scenic escape from the everyday.</motion.p>
+                <motion.button className="button-2" initial={{ opacity: 0, y: 45 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .8 }} viewport={{ margin: "-50px", once: true }} >LEARN MORE</motion.button>
             </div>
         </section>
   )
