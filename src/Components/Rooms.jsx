c0874009e52b7c1decba096a8ebe10fb1e85c223
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-11 23:06:13 -0500

    add rooms photo

--- src/Components/Rooms.jsx
+++ src/Components/Rooms.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 function Rooms() {
   return (
     <section className="rooms">
-        Rooms
-    </section>
+          <div className="home-grid">
+                <motion.img initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-200px", once: true }} className="rooms-image" alt="Sunrise on Villa Noe Beach sign." src="cottage.jpg" />
+                <motion.h2 className="small-font" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} >OUR ROOMS</motion.h2>
+                <motion.p className="paragraph-2" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} ></motion.p>
+                <motion.button className="button" initial={{ opacity: 0, y: 45 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .8 }} viewport={{ margin: "-50px", once: true }} >LEARN MORE</motion.button>
+          </div>
+      </section>
   )
 }
 
