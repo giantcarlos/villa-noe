a265027e6472d710bb6e0439cf082a879cc8a5bd
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-12 21:35:07 -0500

    change rooms section to paragraph form

--- src/Components/Rooms.jsx
+++ src/Components/Rooms.jsx
@@ -7,13 +7,7 @@ function Rooms() {
           <div className="home-grid">
                 <motion.img className="rooms-image" initial={{ opacity: 0, y: 45 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-200px", once: true }} alt="Cottages at Villa Noe." src="cottage.jpg" />
                 <motion.h2 className="small-font-3" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} >OUR ROOMS</motion.h2>
-                <motion.div className="bullets" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} >
-                  <ul>RUSTIC COTTAGES</ul>
-                  <ul>BEACHFRONT VIEWS</ul>
-                  <uL>AIR CONDITIONED ROOMS</uL>
-                  <ul>OPEN AIR CABANAS</ul>
-                  <ul>CAMPING TENTS</ul>
-                </motion.div>
+                <motion.p className="paragraph-2" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} >Villa Noe Beach offers a range of accommodations to suit every traveler. Stay in a rustic cottage, wake up to the ocean in a beachfront room, or enjoy the comfort of an air-conditioned space. For a more open experience, relax in an open-air cabana or camp under the stars with our tent options.</motion.p>
                 <motion.button className="button-3" initial={{ opacity: 0, y: 45 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .8 }} viewport={{ margin: "-50px", once: true }} >LEARN MORE</motion.button>
           </div>
       </section>
