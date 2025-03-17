01582db30b254a5693311b3fb3e2ae95f3353c44
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-17 14:22:49 -0500

    add popup menu nav options and smooth scroll

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 function Contact() {
   return (
-    <section className="colors-1">
+    <section id="contact" className="colors-1">
       <div className="home-grid">
               <motion.img className="contact-image" initial={{ opacity: 0, y: 45 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-200px", once: true }} alt="Cottages at Villa Noe." src="jump.jpg" />
               <motion.h2 className="small-font-5" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1.125 }} viewport={{ margin: "-100px", once: true }} >BOOK NOW</motion.h2>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -16,6 +16,18 @@ function NavBar() {
         setOpen(false)
     }
 
+    const scrollWithOffset = (el) => {
+      const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+      const yOffset = -80; 
+      window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+  }
+
+  const scrollWithOffset2 = (el) => {
+    const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+    const yOffset = -40; 
+    window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+}
+
   const reveal = {
     hidden: { y: -100 },
     show: { y: 0 },
@@ -29,11 +41,11 @@ function NavBar() {
                 <Hamburger toggled={isOpen} toggle={setOpen}/>
                 </div>
                 <div className={active} onClick={navOff}>
-                    <HashLink to="#" className="links" >HOME ➤</HashLink>
-                    <HashLink to="#lineup" className="links" >LINEUP ➤</HashLink>
-                    <HashLink to="#tickets" className="links" >TICKETS ➤</HashLink>
-                    <HashLink to="#merch" className="links" >MERCH ➤</HashLink>
-                    <HashLink to="#press" className="links" >PRESS ➤</HashLink>
+                    <HashLink to="#" className="links" scroll={el => scrollWithOffset(el)} >HOME</HashLink>
+                    <HashLink to="#lineup" className="links" >ABOUT</HashLink>
+                    <HashLink to="#tickets" className="links" >ROOMS</HashLink>
+                    <HashLink to="#merch" className="links" >AMENITIES</HashLink>
+                    <HashLink to="#contact" className="links" scroll={el => scrollWithOffset2(el)} >CONTACT</HashLink>
                 </div>
             <div className="numbers">0906-519-7126 / 0921-784-9062</div>
       </motion.nav>
