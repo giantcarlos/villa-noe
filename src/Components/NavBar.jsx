8855d91e49ca654ad906abc8b73a5290e64ef975
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-17 15:28:40 -0500

    fix nav linksg

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -16,23 +16,6 @@ function NavBar() {
         setOpen(false)
     }
 
-    const container = {
-      hidden: { opacity: 0 },
-      show: {
-        opacity: 1,
-        transition: {
-          delayChildren:.5,
-          staggerChildren: 0.1125
-        }
-      }
-    }
-
-    const item = {
-      hidden: { opacity: 0 },
-      show: { opacity: 1 }
-    }
-
-
     const scrollWithOffset = (el) => {
       const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
       const yOffset = -80; 
@@ -63,16 +46,16 @@ function NavBar() {
         <div className="hamburger" onClick={navToggle}>
                 <Hamburger toggled={isOpen} toggle={setOpen}/>
                 </div>
-                <motion.div variants={container} className={active} onClick={navOff}>
-                    <motion.HashLink variants={item} to="#" className="links" scroll={el => scrollWithOffset(el)} >HOME</motion.HashLink>
-                    <motion.HashLink variants={item} to="#about" className="links" scroll={el => scrollWithOffset3(el)} >ABOUT</motion.HashLink>
-                    <motion.HashLink variants={item} to="#rooms" className="links" scroll={el => scrollWithOffset(el)} >ROOMS</motion.HashLink>
-                    <motion.HashLink variants={item} to="#amenities" className="links" scroll={el => scrollWithOffset(el)} >AMENITIES</motion.HashLink>
-                    <motion.HashLink variants={item} to="#contact" className="links" scroll={el => scrollWithOffset2(el)} >CONTACT</motion.HashLink>
-                    <motion.div variants={item} className="webmaster">Website designed and maintained by&nbsp;
-                      <a className="portfolio" href='https://www.giantcarlos.com/' target="_blank">Gian Carlos</a>
-                    </motion.div>
-                </motion.div>
+                <div className={active} onClick={navOff}>
+                    <HashLink to="#" className="links" scroll={el => scrollWithOffset(el)} >HOME</HashLink>
+                    <HashLink to="#about" className="links" scroll={el => scrollWithOffset3(el)} >ABOUT</HashLink>
+                    <HashLink to="#rooms" className="links" scroll={el => scrollWithOffset(el)} >ROOMS</HashLink>
+                    <HashLink to="#amenities" className="links" scroll={el => scrollWithOffset(el)} >AMENITIES</HashLink>
+                    <HashLink to="#contact" className="links" scroll={el => scrollWithOffset2(el)} >CONTACT</HashLink>
+                    <div className="webmaster">Website designed and maintained by&nbsp;
+                      <a className="portfolio"href='https://www.giantcarlos.com/' target="_blank">Gian Carlos</a>
+                    </div>
+                </div>
             <div className="numbers">0906-519-7126 / 0921-784-9062</div>
       </motion.nav>
   )
