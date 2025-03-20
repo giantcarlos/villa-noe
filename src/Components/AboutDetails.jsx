3e5137b5ffdacc37f66b737beb9d3cc0541a9691
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-19 20:27:43 -0500

    add CSS styling to AboutDetails

--- src/Components/AboutDetails.jsx
+++ src/Components/AboutDetails.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 function AboutDetails() {
   return (
-    <div className="about-details">
-        <p>
-        Villa Noe Beach was originally a private beach house, named in honor of the late Noe V. Taiño, a World War II veteran and father of the current owners. The land, located on Cagbalete Island, was passed down through generations, tracing back to Don Benedicto Pansacola, whose daughter, Doña Maria, married Don Silverio Taiño, a former El Presidente of Mauban. Among their seven children was Noe Taiño.
-
-Noe Taiño studied law at the University of Santo Tomas, served as a soldier during World War II, and later worked at the National Treasury of the Philippines. He married Cornelia Javaluyas, a teacher, and together they raised their family.
-
-In 1990, their eldest daughter, Lorie Taiño Carlos, built a beach house on the family land, serving as a private summer retreat for the Taiño clan and close friends. By 2012, the family began developing the property into a public beach resort, transforming it into the Villa Noe Beach known today—a peaceful getaway that welcomes visitors to experience the natural beauty of Cagbalete Island.  
-        </p>
-    </div>
+    <section className="colors-2">
+        <div className="about-details">
+            <p>Villa Noe Beach was originally a private beach house, named in honor of the late Noe V. Taiño, a World War II veteran and father of the current owners. The land, located on Cagbalete Island, was passed down through generations, tracing back to Don Benedicto Pansacola, whose daughter, Doña Maria, married Don Silverio Taiño, a former El Presidente of Mauban. Among their seven children was Noe Taiño.</p>
+            <p>Noe Taiño studied law at the University of Santo Tomas, served as a soldier during World War II, and later worked at the National Treasury of the Philippines. He married Cornelia Javaluyas, a teacher, and together they raised their family.</p>
+            <p>In 1990, their eldest daughter, Lorie Taiño Carlos, built a beach house on the family land, serving as a private summer retreat for the Taiño clan and close friends. By 2012, the family began developing the property into a public beach resort, transforming it into the Villa Noe Beach known today—a peaceful getaway that welcomes visitors to experience the natural beauty of Cagbalete Island.  </p>
+        </div>
+    </section>
   )
 }
 
