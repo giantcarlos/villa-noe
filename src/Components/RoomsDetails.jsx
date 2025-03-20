c1b0114cd41e65349103d0bceded173b5585b395
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-20 14:26:47 -0500

    add room details

--- src/Components/RoomsDetails.jsx
+++ src/Components/RoomsDetails.jsx
@@ -36,10 +36,38 @@ function RoomsDetails() {
                 <motion.div variants={item2}>
                     <Link className="return" to="/">← Return to home</Link>
                 </motion.div>
-                <motion.div variants={item2} className="details-title">A History of Villa Noe Beach</motion.div>
-                <motion.p variants={item2}>Villa Noe Beach was originally a private beach house, named in honor of the late Noe V. Taiño, a World War II veteran and father of the current owners. The land, located on Cagbalete Island, was passed down through generations, tracing back to Don Benedicto Pansacola, whose daughter, Doña Maria, married Don Silverio Taiño, a former El Presidente of Mauban. Among their seven children was Noe Taiño.</motion.p>
-                <motion.p variants={item2}>Noe Taiño studied law at the University of Santo Tomas, served as a soldier during World War II, and later worked at the National Treasury of the Philippines. He married Cornelia Javaluyas, a teacher, and together they raised their family.</motion.p>
-                <motion.p variants={item2}>In 1990, their eldest daughter, Lorie Taiño Carlos, built a beach house on the family land, serving as a private summer retreat for the Taiño clan and close friends. By 2012, the family began developing the property into a public beach resort, transforming it into the Villa Noe Beach known today—a peaceful getaway that welcomes visitors to experience the natural beauty of Cagbalete Island.</motion.p>
+                <motion.div variants={item2} className="details-title">Our Rooms</motion.div>
+                <motion.figure variants={item2} className="couple-image">
+                    <img src="aircon.jpeg" alt="Noe and Cornelia Taino."></img>
+                    <figcaption className="couple-cap">Air-Conditioned Room</figcaption>
+                </motion.figure>
+                <motion.div variants={item2}>
+                  <ul>Air-Conditioned Rooms:</ul>
+                  <ul>Comes in 2 pax, 3-4 pax, and 5-6 pax.</ul>
+                </motion.div>
+                <motion.figure variants={item2} className="couple-image">
+                    <img src="cottage.jpg" alt="Rustic fan cottages."></img>
+                    <figcaption className="couple-cap">Rustic Fan Rooms</figcaption>
+                </motion.figure>
+                <motion.div variants={item2}>
+                  <ul>Fan Rooms:</ul>
+                  <ul>Comes in 2 pax, 3-4 pax, 5-6 pax, 7-8 pax, 9-10 pax, 11-12 pax.</ul>
+                  <ul>Deluxe Fan Rooms:</ul>
+                  <ul>Comes in 13-16, pax 17-20 pax.</ul>
+                </motion.div>
+                <motion.div variants={item2}>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                </motion.div>
+                <motion.div variants={item2}>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                </motion.div>
+                  
             </motion.div>
         </motion.section>
   )
