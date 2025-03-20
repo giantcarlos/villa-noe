388177718dbefc1e93a37e69ca011cd068fd664d
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-19 20:59:43 -0500

    fix about button

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,8 +10,8 @@ function About() {
                 <motion.img className="about-image" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} viewport={{ margin: "-200px", once: true }} alt="Aerial view of Cagbalete Island." src="cagbalete.jpg" />
                 <motion.h2 className="small-font-2" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} viewport={{ margin: "-100px", once: true }} >WELCOME TO VILLA NOE BEACH</motion.h2>
                 <motion.p className="paragraph" initial={{ opacity: 0, y: 65 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: 1 }} viewport={{ margin: "-100px", once: true }} >Nestled on the pristine shores of Cagbalete Island in Mauban, Quezon, Villa Noe Beach offers a peaceful retreat with clear waters, white sand, and breathtaking sunrises. Whether you’re here to unwind by the beach, explore the island’s marine life, or simply enjoy the tranquility, our resort provides a comfortable and scenic escape from the everyday.</motion.p>
-                <Link to="/about">
-                <motion.button className="button-2" initial={{ opacity: 0, y: 45 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .75 }} viewport={{ margin: "-50px", once: true }} >LEARN MORE</motion.button>
+                <Link to="/about" className="button-2">
+                <motion.button initial={{ opacity: 0, y: 45 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .75 }} viewport={{ margin: "-50px", once: true }} >LEARN MORE</motion.button>
                 </Link>
             </div>
         </section>
