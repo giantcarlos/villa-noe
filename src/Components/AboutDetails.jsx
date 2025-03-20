338ead181c3edf9ca749219a02968fded2a73250
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-19 20:54:07 -0500

    add return button to AboutDetails

--- src/Components/AboutDetails.jsx
+++ src/Components/AboutDetails.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function AboutDetails() {
   return (
     <section className="colors-2">
         <div className="about-details">
+            <Link to="/" className="return">Return to home</Link>
             <div className="details-title">A History of Villa Noe Beach</div>
             <p>Villa Noe Beach was originally a private beach house, named in honor of the late Noe V. Taiño, a World War II veteran and father of the current owners. The land, located on Cagbalete Island, was passed down through generations, tracing back to Don Benedicto Pansacola, whose daughter, Doña Maria, married Don Silverio Taiño, a former El Presidente of Mauban. Among their seven children was Noe Taiño.</p>
             <p>Noe Taiño studied law at the University of Santo Tomas, served as a soldier during World War II, and later worked at the National Treasury of the Philippines. He married Cornelia Javaluyas, a teacher, and together they raised their family.</p>
