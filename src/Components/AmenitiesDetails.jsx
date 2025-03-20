49d076cf8ecb11234da01b8a9ec6fd9d0be1db33
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-20 13:50:31 -0500

    add amenities prices

--- src/Components/AmenitiesDetails.jsx
+++ src/Components/AmenitiesDetails.jsx
@@ -36,10 +36,27 @@ function AmenitiesDetails() {
                 <motion.div variants={item2}>
                     <Link className="return" to="/">← Return to home</Link>
                 </motion.div>
-                <motion.div variants={item2} className="details-title">A History of Villa Noe Beach</motion.div>
-                <motion.p variants={item2}>Villa Noe Beach was originally a private beach house, named in honor of the late Noe V. Taiño, a World War II veteran and father of the current owners. The land, located on Cagbalete Island, was passed down through generations, tracing back to Don Benedicto Pansacola, whose daughter, Doña Maria, married Don Silverio Taiño, a former El Presidente of Mauban. Among their seven children was Noe Taiño.</motion.p>
-                <motion.p variants={item2}>Noe Taiño studied law at the University of Santo Tomas, served as a soldier during World War II, and later worked at the National Treasury of the Philippines. He married Cornelia Javaluyas, a teacher, and together they raised their family.</motion.p>
-                <motion.p variants={item2}>In 1990, their eldest daughter, Lorie Taiño Carlos, built a beach house on the family land, serving as a private summer retreat for the Taiño clan and close friends. By 2012, the family began developing the property into a public beach resort, transforming it into the Villa Noe Beach known today—a peaceful getaway that welcomes visitors to experience the natural beauty of Cagbalete Island.</motion.p>
+                <motion.div variants={item2} className="details-title">Amenities and Resort Activities</motion.div>
+                <motion.div variants={item2}>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                  <ul></ul>
+                </motion.div>
+                <motion.div variants={item2}>
+                  <ul>Beach Volleyball - 100/hour</ul>
+                  <ul>Billiards - 300/hour</ul>
+                  <ul>Catering - Varies*</ul>
+                  <ul>Frisbee - 50/hour</ul>
+                  <ul>Island Tours - Varies*</ul>
+                  <ul>Massage - 400</ul>
+                  <ul>Kayaks - 300/hour</ul>
+                  <ul>Paddleboards - 250/hour</ul>
+                  <ul>Skimboards - 200/hour</ul>
+                  <ul>*For the latest prices and availability, send us a message on Facebook or call us at 0906-519-7126 / 0921-784-9062.</ul>
+                </motion.div>
             </motion.div>
       </motion.section>
   )
