84d95d3938004e19c4f136a41a91d1fb4d745c70
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-20 13:01:13 -0500

    add scroll useEffect to AmenitiesDetails

--- src/Components/AmenitiesDetails.jsx
+++ src/Components/AmenitiesDetails.jsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 function AmenitiesDetails() {
+
+  useEffect(()=>{
+              window.scrollTo(0, 0);
+            },[])
+      
+          const item = {
+              hidden: { y: -1000 },
+              show: { y: 0 },
+              trans: { }
+          }
+      
+          const item2 = {
+              hidden: { y: 12.5, opacity: 0 },
+              show: { y: 0, opacity: 1 }
+            }
+      
+          const container = {
+              hidden: { opacity: 0 },
+              show: {
+                opacity: 1,
+                transition: {
+                  delayChildren: 1.25,
+                  staggerChildren: 0.125
+                }
+              }
+            }
+
   return (
     <motion.section variants={item} initial="hidden" animate="show" transition={{duration: 1, delay: .25}} className="colors-2">
             <motion.div variants={container} initial="hidden" animate="show" className="details">
@@ -14,7 +41,7 @@ function AmenitiesDetails() {
                 <motion.p variants={item2}>Noe Taiño studied law at the University of Santo Tomas, served as a soldier during World War II, and later worked at the National Treasury of the Philippines. He married Cornelia Javaluyas, a teacher, and together they raised their family.</motion.p>
                 <motion.p variants={item2}>In 1990, their eldest daughter, Lorie Taiño Carlos, built a beach house on the family land, serving as a private summer retreat for the Taiño clan and close friends. By 2012, the family began developing the property into a public beach resort, transforming it into the Villa Noe Beach known today—a peaceful getaway that welcomes visitors to experience the natural beauty of Cagbalete Island.</motion.p>
             </motion.div>
-        </motion.section>
+      </motion.section>
   )
 }
 
