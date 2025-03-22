d1d38ab6b171b525a573ffc413635fbc738d38f8
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-21 22:38:54 -0500

    return strictmode and move browserrouter to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
 )
