b5ee1c505bf7a6f2942ee5c0e7b00981054a1cc9
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-03-21 20:39:21 -0500

    add Facebook link to contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -24,7 +24,8 @@ function Contact() {
       <motion.div className="home-grid" variants={container} initial="hidden" whileInView="show" viewport={{ amount: .45, once: true }}>
               <motion.img className="contact-image" variants={item} alt="Cottages at Villa Noe." src="jump.jpg" />
               <motion.h2 className="small-font-5" variants={item} >BOOK NOW</motion.h2>
-              <motion.p className="paragraph-4" variants={item} >For reservations or inquiries, message us on our Facebook page or call us at 0906-519-7126 or 0921-784-9062.</motion.p>
+              <motion.p className="paragraph-4" variants={item} >For reservations or inquiries, message us on our&nbsp;
+              <a className="details-link" href='https://www.facebook.com/VillaNoeBeach/' target="_blank">Facebook</a> page or call us at 0906-519-7126 or 0921-784-9062.</motion.p>
               <motion.a href="https://www.facebook.com/VillaNoeBeach/" target="_blank" className="facebook-image" variants={item}><img src="facebook.png" alt="Find us on facebook banner image." width="300" /></motion.a>
         </motion.div>
     </section>
